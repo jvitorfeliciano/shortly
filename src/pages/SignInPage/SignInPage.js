1691245642d91e47e33d5b5d7d821f08946f0727
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2022-12-22 20:36:38 -0300

    feat: add SignIn Page

--- src/pages/SignInPage/SignInPage.js
+++ src/pages/SignInPage/SignInPage.js
@@ -1,10 +1,18 @@
+import Button from "../../components/Button/Button";
 import Container from "../../components/Container/Container";
+import Form from "../../components/Form/Form";
+import Input from "../../components/Input /Input";
 import Logo from "../../components/Logo/Logo";
 
 export default function SignInPage() {
   return (
     <Container>
-      <Logo></Logo>
+      <Logo />
+      <Form>
+        <Input type="email" placeholder="E-mail" required />
+        <Input type="password" placeholder="Senha" required />
+        <Button>Entrar</Button>
+      </Form>
     </Container>
   );
 }
