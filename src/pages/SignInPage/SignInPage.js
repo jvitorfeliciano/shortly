6d44c5e90037dbfdfb950e8739a95f6b6d783a57
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2022-12-23 09:05:48 -0300

    feat: add button disable when logging in

--- src/pages/SignInPage/SignInPage.js
+++ src/pages/SignInPage/SignInPage.js
@@ -4,18 +4,40 @@ import Container from "../../components/Container/Container";
 import Form from "../../components/Form/Form";
 import Input from "../../components/Input /Input";
 import Logo from "../../components/Logo/Logo";
+import api from "../../services/api";
+import Swal from "sweetalert2";
+import Loader from "../../components/Loader/Loader";
+import { useNavigate } from "react-router-dom";
 
 export default function SignInPage() {
   const [form, setForm] = useState({ email: "", password: "" });
-
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate("/");
   function handleFormChange(e) {
     setForm({ ...form, [e.target.name]: e.target.value });
   }
 
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      await api.signIn(form);
+      setIsLoading(false);
+      navigate("/");
+    } catch (err) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+      });
+      setIsLoading(false);
+    }
+  }
   return (
     <Container>
       <Logo />
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Input
           width={"100%"}
           type="email"
@@ -32,8 +54,13 @@ export default function SignInPage() {
           onChange={handleFormChange}
           required
         />
-        <Button marginTop={"61px"} marginLeft={"0px"}>
-          Entrar
+        <Button
+          marginTop={"61px"}
+          marginLeft={"0px"}
+          type="submit"
+          isLoading={isLoading}
+        >
+          {isLoading ? <Loader size={40} color={"white"} /> : "Entrar"}
         </Button>
       </Form>
     </Container>
