3d28451be6491205ca1f251ad523561d9755562b
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2022-12-23 09:35:35 -0300

    feat: add conditional rendering in the Home Page

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,25 +1,35 @@
+import { useContext } from "react";
 import styled from "styled-components";
 import Button from "../../components/Button/Button";
 import Container from "../../components/Container/Container";
 import Input from "../../components/Input /Input";
 import Logo from "../../components/Logo/Logo";
 import Url from "./Url";
+import UserContext from "../../contexts/UserContext";
+import Ranking from "../../components/Ranking/Ranking";
 
 export default function HomePage() {
+  const { userData } = useContext(UserContext);
+
   return (
     <Container>
       <Logo />
-      <ShortUrl>
-        <Input placeholder="Links que cabem no bolso" />
-        <Button marginTop={"0px"} marginLeft={"70px"} type="submit">
-          Encurtar Link
-        </Button>
-      </ShortUrl>
-      <UrlsContainer>
-        <Url />
-        <Url />
-        <Url />
-      </UrlsContainer>
+      {!userData && <Ranking />}
+      {userData && (
+        <>
+          <ShortUrl>
+            <Input placeholder="Links que cabem no bolso" />
+            <Button marginTop={"0px"} marginLeft={"70px"} type="submit">
+              Encurtar Link
+            </Button>
+          </ShortUrl>
+          <UrlsContainer>
+            <Url />
+            <Url />
+            <Url />
+          </UrlsContainer>
+        </>
+      )}
     </Container>
   );
 }
