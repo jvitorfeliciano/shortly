0ddf31bb4b1f90fe26cb51c161714fd3827c8ce9
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2022-12-23 07:57:12 -0300

    feat: add handle form change function in the Sign Up Page

--- src/pages/SignUpPage/SignUpPage.js
+++ src/pages/SignUpPage/SignUpPage.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Button from "../../components/Button/Button";
 import Container from "../../components/Container/Container";
 import Form from "../../components/Form/Form";
@@ -5,14 +6,49 @@ import Input from "../../components/Input /Input";
 import Logo from "../../components/Logo/Logo";
 
 export default function SignUpPage() {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  console.log(form);
+  function handleFormchange(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+
   return (
     <Container>
       <Logo />
       <Form>
-        <Input type="text" placeholder="Nome" required />
-        <Input type="email" placeholder="E-mail" required />
-        <Input type="password" placeholder="Senha" required />
-        <Input type="password" placeholder="Confirmar senha" required />
+        <Input
+          type="text"
+          placeholder="Nome"
+          name="name"
+          onChange={handleFormchange}
+          required
+        />
+        <Input
+          type="email"
+          placeholder="E-mail"
+          name="email"
+          onChange={handleFormchange}
+          required
+        />
+        <Input
+          type="password"
+          placeholder="Senha"
+          name="password"
+          onChange={handleFormchange}
+          required
+        />
+        <Input
+          type="password"
+          placeholder="Confirmar senha"
+          name="confirmPassword"
+          onChange={handleFormchange}
+          required
+        />
         <Button marginTop={"61px"} marginLeft={"0px"} type="submit">
           Criar conta
         </Button>
